f8544e9cccca5b34235745a1d55a1c3e6d8d01c1
Tony1324 <61944932+Tony1324@users.noreply.github.com>
2025-05-18 10:22:31 -0400

    initial functional website

---
+++ index.js
@@ -0,0 +1,14 @@
+let contactContainer = document.querySelector('.contact-container');
+let contactButton = document.querySelector('#contact-link');
+
+
+contactButton.addEventListener('click', () => {
+    contactContainer.classList.toggle('contact-closed');
+})
+
+contactContainer.addEventListener('click', (e) => {
+    // close when clicked anywhere except links
+    if (e.target.tagName !== 'A') {
+       contactContainer.classList.toggle('contact-closed');
+    }
+})
