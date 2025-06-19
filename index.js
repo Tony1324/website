af84907286e3d4e34b11094d694fe6445186ce78
Tony1324 <61944932+Tony1324@users.noreply.github.com>
2025-06-18 22:36:03 -0400

    modify box shadow and blur

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let contactContainer = document.querySelector('.contact-container');
-let contactButton = document.querySelector('#contact-link');
+let contactContainer = document.querySelector(".contact-container");
+let contactButton = document.querySelector("#contact-link");
 
+contactButton.addEventListener("click", () => {
+  contactContainer.classList.toggle("contact-closed");
+});
 
-contactButton.addEventListener('click', () => {
-    contactContainer.classList.toggle('contact-closed');
-})
-
-contactContainer.addEventListener('click', (e) => {
-    // close when clicked anywhere except links
-    if (e.target.tagName !== 'A') {
-       contactContainer.classList.toggle('contact-closed');
-    }
-})
+contactContainer.addEventListener("click", (e) => {
+  // close when clicked anywhere except links
+  if (e.target.tagName !== "A") {
+    contactContainer.classList.toggle("contact-closed");
+  }
+});
